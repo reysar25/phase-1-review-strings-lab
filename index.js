9748c2473f843fc19e149fe72995669555986666
reysar25 <reyhanaintisaar@gmail.com>
2025-03-09 14:08:43 +0300

    review-string-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Reyhana Intisaar';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
